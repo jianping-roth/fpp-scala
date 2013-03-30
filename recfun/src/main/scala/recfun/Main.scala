62923126ae09ec40cc2b43bc3a05091d41668f99
jianping roth <jianping.roth@gmail.com>
2013-03-29 18:09:53 -0700

    used recursion only to compute the number of changes

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -23,27 +23,23 @@ object Main {
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-    var total = 0
-
-    def doBalance(chars: List[Char]): Boolean = {
-      if (chars.isEmpty) return true
-      else if (chars.head == '(') total = total + 1;
-      else if (chars.head == ')') {
-        total = total - 1
-        if (total < 0) return false
-      }
-      doBalance(chars.tail)
+    def doBalance(numOpens: Int, chars: List[Char]): Boolean = {
+      if (numOpens < 0) false
+      else if (chars.isEmpty) true
+      else if (chars.head == '(') doBalance(numOpens + 1, chars.tail)
+      else if (chars.head == ')') doBalance(numOpens - 1, chars.tail)
+      else doBalance(numOpens, chars.tail)
     }
 
-    doBalance(chars)
+    doBalance(0, chars)
   }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
+  def countChange1(money: Int, coins: List[Int]): Int = {
     def compute(money: Int, coins: List[Int]): Int = {
-      if (money < 0) 0 
+      if (money < 0) 0
       else if (money == 0) 1
       else if (coins.isEmpty) 0
       else {
@@ -54,8 +50,34 @@ object Main {
         total
       }
     }
+
     if (money == 0) 0 else if (coins.isEmpty) 0 else {
-    	compute(money, coins)
+      compute(money, coins)
     }
   }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def compute(total: Int, money: Int, coins: List[Int]): Int = {
+      if (money < 0) total
+      else if (money == 0) total+1
+      else if (coins.isEmpty) total
+      else {
+          compute(total, money - coins.head, coins) + compute(total, money, coins.tail)
+      }
+    }
+
+    if (money == 0) 0 else if (coins.isEmpty) 0 else {
+      compute(0, money, coins)
+    }
+  }
+  
+  def factLoop (total : Int, n : Int) : Int = {
+    if (n == 0) total
+    else factLoop(total*n, n-1)
+  }
+  
+  def fact(n: Int) = factLoop(1, n)
 }
